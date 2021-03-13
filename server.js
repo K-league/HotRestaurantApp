c7332b7a7edbca86d3bd4d24b4bc3146f293bec7
Ashley Lee <ashleyhayjin@iMac.fios-router.home>
2021-03-13 13:20:45 -0600

    server

--- server.js
+++ server.js
@@ -10,10 +10,18 @@ app.use(express.json());
 
 // Data collected for the tables, first 5 will be in tables.
 const tables = [];
-const waitlist = [];
+const waitlist = [{
+    routeName: 'yoda',
+    name: 'Yoda',
+    role: 'Jedi Master',
+    age: 900,
+    forcePoints: 2000,
+    }
+];
 
 
-// routes for the different pages 
+// routes for the different pages erver.js
+
 // app.get
 // 3 different routes: home page, view tables, and make reservation
 // tables.html, home.html, reservation.html
@@ -24,10 +32,19 @@ app.get('/tables', (req,res) => res.sendFile(path.join(__dirname,"tables.html"))
 app.get('/reserve', (req,res) => res.sendFile(path.join(__dirname,"reserve.html")));
 
 // 2 different routes for api table link and api wait list 
-app.get('/api/wailist', (req, res) => res.json(waitlist));
+app.get('/api/wailist', (req, res) => {
+    
+    res.json(waitlist)
+    
+});
 
-app.get('/api/tables', (req, res) => res.json(tables));
+app.get('/api/waitlist', (req, res) => res.json(waitlist));
 // display the tables / app.post will be below here with the routes
+
+app.post('/api/waitlist', (req,res) => {
+    const newWaitlist = req.body;
+    res.json(newWaitlist);
+})
 
 
 app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
